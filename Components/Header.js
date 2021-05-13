dc9728953b92e96c4b824c649fbf19931d06d018
aiacuone <aiacuone@users.noreply.github.com>
2021-05-13 15:50:08 +0100

    minor changes

--- Components/Header.js
+++ Components/Header.js
@@ -29,7 +29,7 @@ export default function Header() {
 
 				<li className={styles.list}>
 					<a href="https://www.linkedin.com/in/adrian-iacuone/" target="_blank">
-						Linkedin
+						LinkedIn
 						<img src={linkedin} className={styles.icon} />
 					</a>
 				</li>
