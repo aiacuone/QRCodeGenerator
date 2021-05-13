1154d983bb7faa94012fcd4bfdc7ddab9c19f6e9
aiacuone <aiacuone@users.noreply.github.com>
2021-05-13 12:40:09 +0100

    deployment issues

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-// const withImages = require('next-images')
+const withImages = require('next-images')
+
+module.exports = withImages()
 // module.exports = {
 
 // 	withImages: require('next-images'),
 // 	distDir: 'out',
 // }
-
-module.exports = withImages()
